2ffd28f037bc40e552b03e7b00b24870b1b4cebb
Chris Grimes <chris.s.grimes@gmail.com>
2022-02-04 16:14:26 -0500

    JSON web token

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
+const authenticate = require('../authenticate');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
 router.post('/signup', (req, res) => {
     User.register(
-        new User({username: req.body.username}),
+        new User({ username: req.body.username }),
         req.body.password,
         err => {
             if (err) {
                 res.statusCode = 500;
                 res.setHeader('Content-Type', 'application/json');
-                res.json({err: err});
+                res.json({ err: err });
             } else {
                 passport.authenticate('local')(req, res, () => {
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
-                    res.json({success: true, status: 'Registration Successfull!'});
+                    res.json({ success: true, status: 'Registration Successfull!' });
                 })
             }
         }
@@ -30,9 +31,10 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-   res.statusCode = 200;
-   res.setHeader('Content-Type', 'application/json');
-   res.json({success: true, status: 'You are successfully logged in!'});
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 router.get('/logout', (req, res, next) => {
