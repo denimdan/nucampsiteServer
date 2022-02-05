a36774b6f4e6dc28080ffc1050ada1ff04949b9d
Chris Grimes <chris.s.grimes@gmail.com>
2022-02-05 11:54:02 -0500

    Week 3 Workshop

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,25 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+    User.find()
+    .then(users => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    })
+    .catch(err => next(err));
 });
 
 router.post('/signup', (req, res) => {
     User.register(
-        new User({ username: req.body.username }),
+        new User({username: req.body.username}),
         req.body.password,
         (err, user) => {
             if (err) {
                 res.statusCode = 500;
                 res.setHeader('Content-Type', 'application/json');
-                res.json({ err: err });
+                res.json({err: err});
             } else {
                 if (req.body.firstname) {
                     user.firstname = req.body.firstname;
@@ -36,12 +42,12 @@ router.post('/signup', (req, res) => {
                     passport.authenticate('local')(req, res, () => {
                         res.statusCode = 200;
                         res.setHeader('Content-Type', 'application/json');
-                        res.json({ success: true, status: 'Registration Successful!' });
+                        res.json({success: true, status: 'Registration Successful!'});
                     });
                 });
             }
         }
-    )
+    );
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
